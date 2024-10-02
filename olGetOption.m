1840268bcf589d2ff992c8f625dcd8c17bcbdfe7
Andreas Sommer <andreas.sommer@iwr.uni-heidelberg.de>
2024-10-02 15:50:18 +0200

    FEATURE: olGetOption can evaluate the default value if needed (saves computation time compared to provided value)

--- olGetOption.m
+++ olGetOption.m
@@ -1,6 +1,6 @@
-function [value, cellarray, foundOption] = olGetOption(cellarray, searchOption, defaultValue)
+function [value, cellarray, foundOption] = olGetOption(cellarray, searchOption, defaultValue, evalDefault)
    % function [value, cellarray, found] = olGetOption(cellarray, searchOption)
-   % function [value, cellarray, found] = olGetOption(cellarray, searchOption, defaultValue)
+   % function [value, cellarray, found] = olGetOption(cellarray, searchOption, defaultValue, evalDefault)
    %
    % Searches cellarray, which is expected to be a cell array of name/value pairs,
    % for specified option and returns the associated value.
@@ -8,15 +8,16 @@ function [value, cellarray, foundOption] = olGetOption(cellarray, searchOption, 
    % Only the first occurence is reported.
    % The searchOption strings are case insensitive.
    %
-   % If the specified value cannot be found, the empty matrix [] is returned.
-   % Try to check first via hasOption()-function, if the
-   % the cell array contains the desired name/value pair.
-   %
+   % If the specified value cannot be found, and no default value is specified, the empty matrix [] is returned.
+   % Try to check first via hasOption()-function, if the the cell array contains the desired name/value pair.
+   % 
    %
    % INPUT:   cellarray --> cell array to process
    %       searchOption --> name of queried option
    %       defaultValue --> value to be used if queried option is not found
    %                        (optional, default is empty matrix [])
+   %        evalDefault --> if specified, defaultValue is evaluated (without arguments)
+   %                        and the result is taken to initialize default value
    %
    % OUTPUT:      value --> value associated with queried option name
    %          cellarray --> cell array with queried option removed
@@ -33,13 +34,13 @@ function [value, cellarray, foundOption] = olGetOption(cellarray, searchOption, 
    %           Mar2017 --> cut the cell array, if two-arg-output
    %           Dec2022 --> add default value 
    %           Jul2024 --> renamed to olGetOption
-   %           Sep2024 --> added found flag
+   %           Sep2024 --> added found flag and possible evaluation of default
    
    % was a default value provided?
    if (nargin<3), defaultValue = []; end
+   if (nargin<4), evalDefault = false; end
    
    % Initialize
-   value = defaultValue;
    olAssertOptionlist(cellarray);
    foundOption = false;
    
@@ -54,7 +55,16 @@ function [value, cellarray, foundOption] = olGetOption(cellarray, searchOption, 
          end
       end
    end
-   
+
+   % if value was not fount, set it to default value
+   if ~foundOption
+      if evalDefault
+         value = defaultValue();
+      else
+         value = defaultValue;
+      end
+   end
+
    % if more than one output argument, remove the queried option (if found)
    if (nargout >= 2) && foundOption
       cellarray = olRemoveOption(cellarray, searchOption);
